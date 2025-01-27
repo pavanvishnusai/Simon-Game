71517a5730e3ce849401a72c5b5b533035cb5d30
PavanVishnuSai <pavanvishnusai@gmail.com>
2025-01-27 18:10:30 -0500

    Simon-Game

---
+++ game.js
@@ -0,0 +1,69 @@
+var buttonColours = ["red", "blue","green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
+
+$(document).keypress(function() {
+    if (!started) {
+        $("#level-title").text("level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("level " + level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function (){
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel){
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+        if (userClickedPattern.length === gamePattern.length){
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }else {   
+        playSound("wrong");
+        $("body").addClass("game-over");
+        $("h1").text("Game Over, Press Any Key to Restart");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        }, 200);
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
